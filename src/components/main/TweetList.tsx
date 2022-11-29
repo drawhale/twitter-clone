4a11f8df6a9cde9ea0464e5da33f7dd2b222cc41
김 동욱 kdw <kdw@spacewalk.tech>
2022-11-29 17:30:25 +0900

    feat: list 에 useIntersect 적용

--- src/components/main/TweetList.tsx
+++ src/components/main/TweetList.tsx
@@ -2,11 +2,15 @@ import styled from "styled-components";
 import { flexColumnBox } from "styles";
 import TweetListItem from "components/main/TweetListItem";
 import useTweetList from "hooks/useTweetList";
+import useIntersect from "hooks/useIntersect";
 
 import type { FC } from "react";
 
 const TweetList: FC = () => {
   const { list, fetchMore } = useTweetList();
+  const intersectRef = useIntersect(() => {
+    fetchMore();
+  });
 
   return (
     <Wrapper>
@@ -14,6 +18,7 @@ const TweetList: FC = () => {
         <TweetListItem key={index} data={item} />
       ))}
       <button onClick={fetchMore}>더보기 버튼</button>
+      <IntersectTarget ref={intersectRef} />
     </Wrapper>
   );
 };
@@ -22,4 +27,8 @@ export default TweetList;
 
 const Wrapper = styled.div`
   ${flexColumnBox};
+`;
+
+const IntersectTarget = styled.div`
+  min-height: 40px;
 `;
